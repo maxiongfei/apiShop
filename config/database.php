b9d208289abff1dcb14015848edb526df1837561
MMA <928202870@qq.com>
2017-09-30 15:32:17 +0800

    add

--- config/database.php
+++ config/database.php
@@ -1,4 +1,5 @@
 <?php
+use think\Env;
 /**
  * @author 马雄飞 <xiongfei.ma@pactera.com>
  * @date   2017/9/30 10:33
@@ -9,15 +10,15 @@ return [
     // 数据库连接DSN配置
     'dsn'             => '',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => Env::get('hostname'),
     // 数据库名
-    'database'        => '',
+    'database'        => Env::get('database'),
     // 数据库用户名
-    'username'        => 'root',
+    'username'        => Env::get('username'),
     // 数据库密码
-    'password'        => '',
+    'password'        => Env::get('password'),
     // 数据库连接端口
-    'hostport'        => '',
+    'hostport'        => Env::get('hostport'),
     // 数据库连接参数
     'params'          => [],
     // 数据库编码默认采用utf8
