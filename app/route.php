85df83f3906686eef9f15d6712db97701b1f3329
ma5694 <18710839146@163.com>
2017-10-05 21:34:54 +0800

    init

--- app/route.php
+++ app/route.php
@@ -9,11 +9,13 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-use think\Route;
-Route::resource(':version/banner','api/:version.Banner');   //注册一个资源路由，对应restful各个方法
 return [
     '__pattern__' => [
         'name' => '\w+',
+    ],
+    '[hello]'     => [
+        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
+        ':name' => ['index/hello', ['method' => 'post']],
     ],
 
 ];
